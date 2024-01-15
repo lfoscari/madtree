27fa0a0d1e4fb67e4bbc0fc1155f6eb0f2bcf921
Luigi Foscari <luigi.foscari@icloud.com>
2024-01-15 17:35:57 +0100

    setup

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+   name="madtree",
+   version="1.0",
+   description="Generate and solve market configuration trees",
+   author="Luigi Foscari",
+   author_email="luigi.foscari@unimi.it",
+   packages=["madtree"],
+   install_requires=["graphviz"],
+)
