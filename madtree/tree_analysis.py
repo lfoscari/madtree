8e139f2f1414b6cb55daae46580307211b8fc3d3
Luigi Foscari <luigi.foscari@icloud.com>
2024-01-25 18:48:16 +0100

    started computing results

--- madtree/tree_analysis.py
+++ madtree/tree_analysis.py
@@ -1,9 +1,12 @@
 from tree_visualization import convert_to_nx, highest_reward_paths, compute_action_distributions
-from tree_generation import deltas_factory, gaussian_densities, generate_tree
+from tree_generation import deltas_factory, generate_tree, gaussian_densities, alternating_densities
 from market_types import Actions
 import random
 
-def interesting_initializations(amount = 1000):
+def interesting_initializations(amount = 100):
+	"""
+	Build random inputs for all the possible interesting initializations of the tree.
+	"""
 	r = lambda: random.randint(1, 100)
 
 	I = [(r(), 0, 0) for _ in range(amount)]
@@ -28,24 +31,32 @@ def interesting_initializations(amount = 1000):
 		"ICP": ICP
 	}
 
-def analize_actions_spread():
-	time_horizon = 10
+def avg_dict(dicts, keys):
+	return {k: sum(d[k] for d in dicts) / len(dicts) for k in keys}
 
+def analize_actions_spread():
+	"""
+	Using the interesting parameters average the distribution of
+	the moves used in the best paths according to the reward of the leaves.
+	"""
+	print("I = inventory, C = cash, P = price")
+	print("(e.g. 'IC' means that only inventory and cash are non-zero)\n")
+
+	time_horizon = 10
 	for name, parameters in interesting_initializations().items():
 		print(name, end=" => ")
 		deltas = deltas_factory(time_horizon, gaussian_densities)
-		
+		par_distr = []
+
 		for par in parameters:
 			tree = generate_tree(time_horizon, deltas, *par)
 
 			nx_tree = convert_to_nx(tree, deltas)
 			paths = highest_reward_paths(nx_tree)
-			distr = []
 
 			for path in paths:
 				edgelist = list(zip(path, path[1:]))
-				distr.append(compute_action_distributions(nx_tree, edgelist))
-				
-		for action in [Actions.BUY, Actions.STAY, Actions.SELL]:
-			print(action.name, sum(d[action] for d in distr) / len(distr), end=" ")
-		print()
+				par_distr.append(compute_action_distributions(nx_tree, edgelist))
+
+		par_results = avg_dict(par_distr, [Actions.BUY, Actions.STAY, Actions.SELL])
+		print(" ".join(f"{a.name}: {p:.3f}" for (a, p) in par_results.items()))
